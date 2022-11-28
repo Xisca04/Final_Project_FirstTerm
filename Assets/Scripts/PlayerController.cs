f8104d6e2beb45350fef9cebc7719ed9df006d36
Xisca04 <xiscacerdavicens@gmail.com>
2022-11-28 10:44:03 +0100

    movement_environment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Movement of the player on the plataform
 
-    // Update is called once per frame
-    void Update()
+    public float speed = 22f;
+    public float turnSpeed = 11f;
+    private float horizontalInput;
+    private float verticalInput;
+    
+    private void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");  // Lateral movement
+        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput); 
         
+        verticalInput = Input.GetAxis("Vertical");  // Forward and backward movement
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput); 
     }
 }
